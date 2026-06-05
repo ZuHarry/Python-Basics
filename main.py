95166ac15ac3905cd3669abaea149dc31ea8f6ec
MUHAMMAD ZUHAIRY BIN RAZALY <zuhairy812@gmail.com>
2026-06-05 12:06:38 +0800

    Add Count String

--- main.py
+++ main.py
@@ -22,13 +22,52 @@ def calculatorMenu():
     print("\nYou entered = ", end="")
     for i in range(len(arr)):
         print(arr[i], end = " ")
+
+def reverseString():
+    print("Reverse String")
+    str = input("Please enter a word or sentence = ")
+
+    reversed_text = "".join(reversed(str))
+    
+    
+def divideString():
+    print("Divide String")
+
+def mergeString():
+    print("Merge String")
+    
+def vowelString():
+    print("Count vowels in String")
+    
+def countString():
+    print("\nCount characters in String")
+    
+    str = input("Enter a word or a sentence = ")
+    
+    count = len(str)
+    
+    print(count)
         
 def stringMenu():
-    print("Welcome to String Menu")
+    print("\nWelcome to String Menu")
     print("1. Reverse a String")
     print("2. Divide a String")
     print("3. Merge two String")
     print("4. Count vowels in String")
+    print("5. Count characters")
+    
+    choiceString = input("Choose = ")
+    
+    if choiceString=="1":
+        reverseString()
+    elif choiceString=="2":
+        divideString()
+    elif choiceString=="3":
+        mergeString()
+    elif choiceString=="4":
+        vowelString()
+    elif choiceString=="5":
+        countString()
     
 
 print("\n==================================")
@@ -36,6 +75,7 @@ print("Welcome to Program")
 print("==================================")
 print("\n1. Calculator")
 print("2. Array")
+print("3. String")
 choice = input("Choose = ")
 
 if choice=="1":
@@ -43,4 +83,4 @@ if choice=="1":
 elif choice=="2":
     calculatorMenu()
 elif choice=="3":
-    calculatorMenu()
+    stringMenu()
